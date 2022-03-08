2d5a6e76f9e1b1d3e5671aaeef393979b3995621
rachannzz <rachanphuyal15@gmail.com>
2022-03-08 13:11:59 +0545

    learning

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1> Blog home <h1>')
+    return render(request,'blog/home.html',)
 
 def about(request):
-    return HttpResponse('<h1>blog about<h1>')
+    return render(request,'blog/about.html',)
