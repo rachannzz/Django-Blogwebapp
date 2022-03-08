8b62ccf139fd915e136f2baef5d34d285c6e404d
rachannzz <rachanphuyal15@gmail.com>
2022-03-08 17:23:21 +0545

    learning

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+posts = [
+    {
+        'author': 'Rachan',
+        'title': 'blog post 1',
+        'content': 'first post',
+        'date_posted': 'feb 24 2022 '
+    },
+
+    {
+        'author': 'sujan',
+        'title': 'blog post 2',
+        'content': 'second post',
+        'date_posted': 'feb 25 2022'
+    }
+]
+
+
 def home(request):
-    return render(request,'blog/home.html',)
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return render(request,'blog/about.html',)
+
+    return render(request, 'blog/about.html', {'title': ' Aboutt'})
